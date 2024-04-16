ca05c984ec2ac16d8caedf691e68fdd94e4c447e
DeniStojanovski <106447554+DeniStojanovski@users.noreply.github.com>
2024-04-16 04:01:40 -0700

    adds last push

--- src/store/modules/auth/getters.js
+++ src/store/modules/auth/getters.js
@@ -1,13 +1,11 @@
 export default {
-  getters: {
-    userId(state) {
-      return state.userId;
-    },
-    supId(state) {
-      return state.supId;
-    },
-    workoutId(state) {
-      return state.workoutId;
-    },
+  userId(state) {
+    return state.userId;
+  },
+  supId(state) {
+    return state.supId;
+  },
+  workoutId(state) {
+    return state.workoutId;
   },
 };
